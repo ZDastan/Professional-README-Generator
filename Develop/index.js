b840ddc5809270ac8b5819a30d491c9c66e7bd5f
zehra <zehradastan@hotmail.com>
2023-02-15 13:16:38 +0000

    question added to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,36 @@
 // TODO: Include packages needed for this application
+const generateMarkdown =require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your project title?',
+      
+    },
+    {
+      type: 'input',
+      message: 'Please enter description, installation instructions, usage information, contribution guidelines, and test instructions?',
+      name: 'sections',
+     
+    },
+    {
+      type: 'checkbox',
+      name: 'license',
+      message: 'What is your preferred license?',
+      choices:[
+        "APACHE 2.O",
+        "BSD 3",
+        "GVL-GPL 3.0",
+        "MIT",
+        "None"
+    ],
+ },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
