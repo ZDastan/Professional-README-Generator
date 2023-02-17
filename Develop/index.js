8338e72bf00f640382f0231461c74b76a363194d
zehra <zehradastan@hotmail.com>
2023-02-17 13:33:43 +0000

    html file asdded

--- Develop/index.js
+++ Develop/index.js
@@ -8,16 +8,28 @@ const questions = [
    
     {
       type: 'input',
-      name: 'name',
+      name: 'title',
       message: 'What is your project title?',
       
     },
     {
       type: 'input',
-      message: 'Please enter description, installation instructions, usage information, contribution guidelines, and test instructions?',
-      name: 'sections',
+      message: 'Please provide a short description explaining the what, why, and how of your project?',
+      name: 'description',
      
     },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+        name: 'installation',
+       
+      },
+      {
+        type: 'input',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+        name: 'usage',
+       
+      },
     {
       type: 'checkbox',
       name: 'license',
@@ -30,6 +42,12 @@ const questions = [
         "None"
     ],
  },
+ {
+    type: 'input',
+    message: 'Please enter your GitHub username',
+    name: 'question',
+   
+  },
   ];
 
 // TODO: Create a function to write README file
